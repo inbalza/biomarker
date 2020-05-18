bfa9c6b6db22b1175de63d840c09da04bce4e58e
Inbal Zaltsman <59653665+inbalza@users.noreply.github.com>
2020-05-18 21:57:47 +0200

    added extract_cbd function

--- resource_ext.py
+++ resource_ext.py
@@ -2,8 +2,32 @@
 
 import pandas as pd
 import numpy as np
-import os 
 
+'''
+Containing functions to be used for extracting biomarker information from different resources
+'''
+
+source_ids = {
+        "Tissue":"UBERON_0000479",
+        "Urine":"UBERON_0001088", 
+        "Blood":"UBERON_0000178",
+        "Saliva":"UBERON_0001836",
+        "Feces":"UBERON_0001988"
+    }
+
+usage = {
+        'Diagnosis':'DiagnosticBM',
+        'Prediction of response to treament':'PredictiveBM',
+        'Early diagnosis':'DiagnosticBM',
+        'Indicator of physiological process':'DiagnosticBM',
+        'Classification':'DiagnosticBM',
+        'Prognosis':'PrognosticBM',
+        'Indicator of severity':'PrognosticBM',
+        'Staging':'PrognosticBM',
+        'Diease progression':'PrognosticBM',
+        'Risk factor':'RiskBM',
+        'Treatment':'PredictiveBM'
+        }
 
 def extract_upbd(input_file,output_file):
     """
@@ -18,42 +42,30 @@ def extract_upbd(input_file,output_file):
     subset = upbd[['Protein name','Protein ID','Biomarker usage','Treatment','Disease','Disease ID','In a panel','Experiment','Pmid']]
 
     # Converting usage to fit the biomarker model
-    usage = {
-        'Diagnosis':'DiagnosticBM',
-        'Prediction of response to treament':'PredictiveBM',
-        'Early diagnosis':'DiagnosticBM',
-        'Indicator of physiological process':'DiagnosticBM',
-        'Classification':'DiagnosticBM',
-        'Prognosis':'PrognosticBM',
-        'Indicator of severity':'PrognosticBM',
-        'Staging':'PrognosticBM',
-        'Diease progression':'PrognosticBM',
-        'Risk factor':'RiskBM'
-        }
     for key,value in usage.items():
         subset["Biomarker usage"] = subset["Biomarker usage"].replace({key:value},regex=True)
 
-    #print(subset['Biomarker usage'].value_counts())
-    #print(subset.shape)
-    #print(subset.isnull().sum())
-
     # Remove biomarkers with no inforamtion about experiment type, usage or whether it's a singel or a panel
+    nrow_before = subset.shape[0]
     subset = subset.dropna(subset = ['Experiment', 'Biomarker usage','In a panel'])
+    nrow_after = subset.shape[0]
+    dropped = nrow_before-nrow_after
+    dropped_per = round(dropped/nrow_before*100,2)
+    print("Dropped {} ({}%) rows with missing data over experiment, usage or panel".format(dropped, dropped_per))
 
     # Predictive biomarkers must contain a treatment as well
     #print("Checking for predictive biomarkers with no treatment:")
-    #print(subset[subset['Biomarker usage'] == 'PredictiveBM'].isnull().sum())
+    #print(subset[subset['Biomarker usage'] == 'PredictiveBM'].isnull().sum()) 
 
     '''
     Adding relevant columns: 
     Source = Urine, Molecular type = Protein
-    The dataset does not contain information about technology or evidence level
-    For now, 'experimental' status will be assigned to all biomarkers and technology will not be specified
+    The dataset does not contain information about technology or evidence level - will not be added for now
     '''
     subset['Type'] = 'MolecularBM'
-    subset['Source'] = 'http://purl.obolibrary.org/obo/UBERON_0001088'
-    subset['Molecular type'] = 'http://ns.ontoforce.com/ontologies/integration_ontology#Protein'
-    subset['Evidence level'] = 'Experimental'
+    subset['Source'] = 'Urine'
+    subset['Source ID'] = source_ids['Urine']
+    subset['Molecular type'] = 'Protein'
 
     # Renaming columns to fit the biomarker model terminology 
     subset.rename(columns={'Protein name':'Name','Protein ID':'Molecular ID','Biomarker usage':'Usage','Experiment':'Assay/Technology'}, inplace=True)
@@ -61,17 +73,19 @@ def extract_upbd(input_file,output_file):
     # Write to csv file
     new_index = [
         'Name','Usage','Disease','Disease ID','In a panel','Evidence level',
-        'Type','Source','Assay/Technology','Pmid','Molecular type','Molecular ID','Treatment']
-    #subset.reindex(new_index)
+        'Type','Source','Source ID','Assay/Technology','Pmid','Molecular type','Molecular ID','Treatment']
+
     print("Writing to "+output_file)
     subset.to_csv(output_file, columns = new_index)
 
-def extract_resdb(output_file):
-    resdb_url = "http://resmarkerdb.org/static/biomarker/files/2018_TREC_Precision_Medicine_Challenge_Data.csv"
-    resdb = pd.read_csv(resdb_url, sep='\t')
-    resdb.to_csv(output_file)
-
 def extract_oncomx(output_file):
+    '''
+    Oncomx contains datasets of FDA-approved or cleared nucleic acid-based human biomarker tests for cancer
+    Each row represents one gene linked to its respective test. 
+    Genes are labeled by relevant identifiers/accessions from UniProtKB, HGNC, and EDRN. 
+    Tests are distinguished by manufacturer, FDA submission ID(s), clinical trial ID(s), and PubMed ID(s).
+    '''
+    # Read and join csv's into one dataframe
     base_url = "http://data.oncomx.org/ln2wwwdata/reviewed/human_cancer_biomarkers_FDA_"
     cancer_types = ["breast","colorectal","lung","ovarian","prostate","melanoma"]
     dfs = []
@@ -81,34 +95,32 @@ def extract_oncomx(output_file):
         dfs.append(pd.read_csv(url))
         
     joined_dfs = pd.concat(dfs,ignore_index=True)
-#    joined_dfs.to_csv(output_dir+"/joined_dfs.csv")
 
+    # Define relevant columns to extract
     cols = [
-        "gene_symbol","test_is_a_panel","doid","actual_use","test_adoption_evidence",
+        "gene_symbol","test_is_a_panel","do_name","doid","actual_use","test_adoption_evidence",
         "specimen_type","test_trade_name","test_manufacturer","pmid",
-        "biomarker_drug","biomarker_description"
+        "biomarker_drug","biomarker_description","biomarker_origin","test_trial_id",
         ]
 
     subset = joined_dfs[cols]
-#    subset.to_csv(output_dir+"/subset.csv")
 
-    #Assigning URIs for disease, specimen, molecular type, molcular id      
-    subset.loc[subset.index, 'doid'] = 'http://purl.obolibrary.org/obo/DOID_' + subset['doid'].astype(str)
-    
+    #Adjusting specimen type and usage to the biomarker model
     #Paraffin block refers to Formalin-Fixed Paraffin-Embedded (FFPE) tissue specimens 
     specimens = {
-        ("Paraffin block","Paraffin Block","Parrafin block","paraffin block","parrafin block", "Fresh Tissue"):"http://purl.obolibrary.org/obo/UBERON_0000479",
-        ("urine","Urine"):"http://purl.obolibrary.org/obo/UBERON_0001088", 
-        ("blood","Blood"):"http://purl.obolibrary.org/obo/UBERON_0000178",
-        ("saliva","Saliva"):"http://purl.obolibrary.org/obo/UBERON_0001836",
-        ("human stool","Stool","stool"):"http://purl.obolibrary.org/obo/UBERON_0001988"
+        ("Paraffin block","Paraffin Block","Parrafin block","paraffin block","parrafin block", "Fresh Tissue"):"Tissue",
+        ("urine","Urine"):"Urine", 
+        ("blood","Blood"):"Blood",
+        ("saliva","Saliva"):"Saliva",
+        ("human stool","Stool","stool"):"Feces"
         }
-    for keys, values in specimens.items():
-        subset["specimen_type"] = subset["specimen_type"].replace(to_replace = keys, value = values,regex=True)
 
+    for key, value in specimens.items():
+        subset["specimen_type"] = subset["specimen_type"].replace(to_replace = key, value = value,regex=True)
+    
     subset["Type"] = 'MolecularBM'
-    subset["Molecular type"] = 'http://ns.ontoforce.com/ontologies/integration_ontology#Gene'
-    subset["Molecular ID"] = 'http://identifiers.org/hgnc.symbol/'+subset["gene_symbol"]
+    subset["Molecular ID"] = subset["gene_symbol"]
+    subset["doid"] = "DOID_" + subset["doid"].astype(str)
     
     usage = {
     "predisposition":"RiskBM",
@@ -120,32 +132,91 @@ def extract_oncomx(output_file):
         subset["actual_use"] = subset["actual_use"].replace(to_replace = keys, value = values,regex=True)
 
     subset.rename(columns={
-        'gene_symbol':'Name','test_is_a_panel':'In a panel','doid':'Disease ID',
+        'gene_symbol':'Name','test_is_a_panel':'In a panel','do_name':'Disease','doid':'Disease ID',
         'actual_use':'Usage','specimen_type':'Source','test_trade_name':'Test trade name',
         'test_manufacturer':'Test manufacturer','pmid':'Pmid','test_adoption_evidence':'Evidence level',
-        'biomarker_drug':'Treatment','biomarker_description':'Description'
+        'biomarker_drug':'Treatment','biomarker_description':'Description','biomarker_origin':'Molecular type','test_trial_id':'Clinical trail ID'
         }, inplace=True)
     
     # Write to csv file
     new_index = [
-        'Name','Usage','Disease ID','In a panel','Evidence level',
-        'Type','Source','Test trade name','Test manufacturer','Pmid','Molecular type','Molecular ID','Treatment']
+        'Name','Usage','Disease','Disease ID','In a panel','Evidence level','Type','Source','Source ID',
+        'Test trade name','Test manufacturer','Pmid','Molecular type','Molecular ID','Treatment','Description','Clinical trail ID']
     
     print("Writing to "+output_file)
     subset.to_csv(output_file, columns = new_index)
 
+def extract_cbd(output_file):
+    """
+    CBD: Colorectal Cancer Biomarker Database http://sysbio.suda.edu.cn/CBD/index.html
+    """
+    url = "http://sysbio.suda.edu.cn/CBD/download/data.xlsx"
+    cbd = pd.read_excel(url, index_col=None)
+    
+    # Extract relevant information
+    subset = cbd[["Biomarker","Categary","Discription","Location","Source","Experiment","Application","PMID","Statictics","Conclusion"]]
 
-# Extract resources
-abspath = os.path.abspath(__file__)
-dname = os.path.dirname(abspath)
-input_file = dname+'/upbd.xls'
-output_file = dname+'/bm_upbd.csv'
+    # Join all columns decscribing the experiment and its results and conclusions into one column
+    subset["Description"] = subset.apply(lambda x: '_'.join(x[["Discription","Statictics","Conclusion"]].dropna().astype(str).values), axis=1)
+    subset = subset.drop(["Discription","Statictics","Conclusion"],axis=1)
 
-#extract_upbd(input_file,output_file)
+    # Dropping rows with no data over Experiment (assay) or Source
+    nrow_before = subset.shape[0]
+    subset = subset.dropna(subset = ["Experiment","Source"])
+    nrow_after = subset.shape[0]
+    dropped = nrow_before-nrow_after
+    dropped_per = round(dropped/nrow_before*100,2)
+    print("Dropped {} ({}%) rows with missing data over experiment or source".format(dropped, dropped_per))
 
-output_file = dname+'/bm_res.csv'
-#extract_resdb(output_file)
+    # Adjusting 
+    molecular_types = {
+        ('Protein','protein'):'Protein',
+        ('DNA','RNA','MicroRNA','LncRNA','LncRNA ','Small nucleolar RNAs (snoRNAs)','Circular RNA'):'Nucleic acid',
+        'Other':np.NaN
+    }
+    for key,value in molecular_types.items():
+        subset["Categary"] = subset["Categary"].replace(to_replace = key, value = value)
 
-output_file = dname+'/bm_oncomx.csv'
-extract_oncomx(output_file)
+    # Replace 'cell line' as source with Tissue
+    subset['Source']=subset['Source'].replace(to_replace=('Cell line','cell line'),value='Tissue',regex=True)
 
+    # Remove duplicate values for source
+    # Adjust usage
+    for index, row in subset.iterrows():
+        src = row['Source'].split(", ")
+        unique_src = list(dict.fromkeys(src))
+        subset.at[index,'Source'] = '_'.join(unique_src)
+
+        apps = row['Application'].split(", ")
+        new_app = usage[apps[0]]
+        if len(apps) > 1:
+            apps.pop(0)
+            for item in apps:
+                new_app = new_app + '|' + usage[item]
+        subset.at[index,'Application'] = new_app
+    
+    # Remove rows with more than once source 
+    nrow_before = subset.shape[0]
+    subset = subset.drop(subset[subset['Source'].str.contains('_')].index)
+    nrow_after = subset.shape[0]
+    dropped = nrow_before-nrow_after
+    dropped_per = round(dropped/nrow_before*100,2)
+    print("Dropped {} ({}%) rows with more than one source".format(dropped, dropped_per))
+
+    # Add source ID
+    subset["Source ID"] = subset["Source"]
+    for key, value in source_ids.items():
+        subset["Source ID"] = subset["Source ID"].replace(to_replace = key, value = value,regex=True)
+
+    subset['Disease'] = 'Colorectal Cancer'
+    subset['Disease ID'] = 'DOID_9256'
+    subset['Type'] = 'MolecularBM'
+
+    subset.rename(columns={
+        'Biomarker':'Name','Application':'Usage','PMID':'Pmid','Categary':'Molecular type'}, inplace=True)
+    
+    # Write to csv file
+    new_index = ['Name','Usage','Disease','Disease ID','Type','Source','Source ID','Experiment','Pmid','Molecular type','Description']
+    
+    print("Writing to "+output_file)
+    subset.to_csv(output_file, columns = new_index)    
