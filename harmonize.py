15637c12f43a1e980155dcc3920d525fc8eee9c8
Inbal Zaltsman <59653665+inbalza@users.noreply.github.com>
2020-06-03 15:56:21 +0200

    add example for creating instances

--- harmonize.py
+++ harmonize.py
@@ -1,53 +1,73 @@
-#!/usr/bin/python3
-
 import os 
 from resource_ext import extract_cbd, extract_oncomx, extract_upbd
-#from owlready2 import *
+import warnings
 
-# Extracting data and saving to csv files
+warnings.filterwarnings('ignore')
+
+##############################################################
+#   Extract data from and adjust it to the biomarker model   #
+##############################################################
+
 abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
 
-input_file = dname+'/upbd.xls'
+# Urine Protein Biomarker Database
+input_file = dname+'/upbd.xls'  
 output_file = dname+'/bm_upbd.csv'
 df_upbd = extract_upbd(input_file,output_file)
 
+# Oncomx FDA Biomarkers
 output_file = dname+'/bm_oncomx.csv'
 df_oncomx = extract_oncomx(output_file)
 
-output_file = dname+'/bm_cbd.csv'
+# Colorectal cancer biomarker database
+output_file = dname+'/bm_cbd.json'
 df_cbd = extract_cbd(output_file)
 
-#output_file = dname+'/bm_ptsd.csv'
-#extract_ptsd(output_file)
-'''
-# Creating new individuals in biomarker ontology
+#######################################################################
+#   Example of creating new instances using biomarker ontology model  #   
+#######################################################################
+
+from owlready2 import *
 bm = get_ontology("file://D:/ontoforce/model/bm_model2.owl").load()
-doid = get_ontology("file://D:/ontoforce/model/doid-merged.owl").load()
-uberon = get_ontology("file://D:/ontoforce/model/uberon.owl").load()
+print("Loading DOID ontology...")
+doid = get_ontology("file://D:/ontoforce/model/doid-merged.owl").load() # downloaded from: https://github.com/DiseaseOntology/HumanDiseaseOntology/tree/master/src/ontology
+print("Loading UBERON ontology...")
+uberon = get_ontology("file://D:/ontoforce/model/uberon.owl").load() # downloaded from https://uberon.github.io/downloads.html
 obo = get_namespace("http://purl.obolibrary.org/obo/")
 
-# Create an instance - example
-desc = 'From FDA The report describes if a woman is at increased risk of developing breast and ovarian cancer_ and if a man is at increased risk of developing breast cancer or may be at increased risk of developing prostate cancer. From EDRN BRCA1 is a nuclear phosphoprotein that functions as a tumor suppressor...[and] thus plays a role in transcription_ DNA repair of double-stranded breaks_ and recombination. Mutations in this gene are responsible for approximately 40% of inherited breast cancers and more than 80% of inherited breast and ovarian cancers.'
-bm1 = RiskBM()
-bm1.is_a.append(MolecularBM)
-bm1.is_a.append(indicatorOf.some(obo.DOID_1612))
-bm1.inPanel.append(True)
-bm1.hasEvidenceLevel.append('Experimental')
-bm1.hasClinicalTrialID.append('NCT01598597')
-bm1.hasDescription.append(desc)
-bm1.is_a.append(measuredIn.some(obo.UBERON_0001836))
-bm1.hasMolecularType.append('somatic mutation')
-bm1.assesedBy.append('Assay')
-bm1.assesedBy.append('Tech')
+print(df_oncomx.columns)
 
-for i in Biomarker.instances():
-    print(i)
-    print(i.is_a)
-    #destroy_entity(i)
+# Creating new individuals in biomarker ontology for OncoMX biomarkers
+nr_inst = 10
+for index, row in df_oncomx.head(nr_inst).iterrows():
+    #print("Biomarker: {}".format(row))
+    name = row['Name']
+    bm_usgae = 'bm.'+row['Usage']
+    bm_type = 'bm.'+row['Type']
+    dis_id = 'obo.'+row['Disease ID']
+    source = 'obo.'+row['Source ID']
+    if row['In a panel'] == 'Y': in_panel = True 
+    else: in_panel = False
 
-# Creating panels
+    new_bm = eval(bm_usgae)(name)
+    new_bm.is_a.append(eval(bm_type))
+    new_bm.inPanel.append(in_panel)
+    new_bm.hasMolecularType.append(row['Molecular type'])
+    
+    # source and disease are classes and not individuals
+    # currently Relation between an individual and a class must be represented by a restriction
+    new_bm.is_a.append(bm.indicatorOf.some(eval(dis_id))) 
+    new_bm.is_a.append(bm.measuredIn.some(eval(source)))
+    
+    new_bm.measuredBy.append(row['Assay/Test'])
+    new_bm.hasEvidenceLevel.append(row['Evidence level'])
+    new_bm.hasClinicalTrialID.append(row['Clinical trail ID'])
+    new_bm.hasDescription.append(row['Description'])
 
-bm.save()
+print("New instances created:")
+for bm in bm.Biomarker.instances():
+    print(bm)
 
-'''
+bm.save(file='D:/ontoforce/model/model_with_instances.owl')
+
